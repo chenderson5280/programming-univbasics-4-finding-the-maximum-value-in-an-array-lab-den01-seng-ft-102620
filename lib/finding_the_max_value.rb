41d41dbf946db950fcad39f3d609ab062b75e9f8
Christopher Henderson <chenderson5280@gmail.com>
2020-10-05 18:21:48 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -10,15 +10,3 @@ count = count + 1
 end
 max_value
 end
-def find_max_value(array)
-  count = 0
-  max_value = -1
-  while count < array.length do
-if max_value < array[count]
-  max_value = array[count]
-
-  end
-count = count + 1
-end
-max_value
-end
